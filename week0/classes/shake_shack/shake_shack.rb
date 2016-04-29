2d2993296a99d86982bc6b474003c532b22e8669
Dieter <odzdit@me.com>
2016-04-28 21:37:31 -0400

    Created the shop, and added two milkshakes to the shop, remember that each class is a bluepring of everything, so we need to create each shop etc...

--- week0/classes/shake_shack/shake_shack.rb
+++ week0/classes/shake_shack/shake_shack.rb
@@ -1,3 +1,23 @@
+class ShakeShop
+	def initialize
+		@shakes = []
+	end
+
+	def add_shake(milkshake)
+		@shakes.push(milkshake)
+	end
+
+	def checkout
+		total = 0
+		@shakes.each do | single_shake |
+			total += single_shake.total_price
+		end
+		puts "Your price for today was: #{total}, have a nice day"
+	end
+end
+
+
+
 class MilkShake
 	def initialize
 		@base_price = 3
@@ -31,7 +51,21 @@ berries = Ingredient.new("Berries", 3)
 caramel = Ingredient.new("Caramel", 3)
 chocolate_chips = Ingredient.new("Chocolate Chips", 3)
 
-my_shake = MilkShake.new
+#My Shake
+my_shake = Milkshake.new
 my_shake.add_ingredient(banana)
+my_shake.add_ingredient(berries)
+
+#Gaby's Shake
+gabys_shake = MilkShake.new
 my_shake.add_ingredient(caramel)
-puts my_shake
+my_shake.add_ingredient(chocolate_chips)
+
+#My new shop
+dieters_shop = ShakeShop.new
+dieters_shop.add_shake(my_shake)
+dieters_shop.add_shake(gabys_shake)
+dieters_shop.checkout
+
+
+
