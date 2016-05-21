0d33edeebd1f6fa15fa5fd3cc8dff16d55de5706
Dieter <odzdit@me.com>
2016-05-20 21:03:17 -0500

    troubleshoot rspec

---
+++ week4/string_calculator/spec/string_calculator_spec.rb
@@ -0,0 +1,9 @@
+require_relative("lib/string_calculator")
+require "spec_helper"
+
+RSpec.describe StringCalculator do
+	it "returns 0 for empty string" do
+		my_calc = StringCalculator.new
+		expect(my_calc.add("")).to eq(0)
+	end
+end
