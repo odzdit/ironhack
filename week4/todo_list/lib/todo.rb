8bb05d5e5f9af87b5190cda59f39becbb8d1a88a
Dieter <odzdit@me.com>
2016-05-21 14:51:41 -0500

    Update on todo list

--- week4/todo_list/lib/todo.rb
+++ week4/todo_list/lib/todo.rb
@@ -1,9 +1,10 @@
 class Task
-    attr_reader :content, :id
+    attr_reader :content, :id, :created_at
     @@current_id = 1
     def initialize(content)
         @content = content
         @id = @@current_id
+        @created_at = Time.now
         @@current_id += 1
     end
 end
--- week4/todo_list/spec/todo_list_spec.rb
+++ week4/todo_list/spec/todo_list_spec.rb
@@ -1,18 +1,58 @@
-require_relative('../lib/todo_list')
-require_relative('../lib/task')
-require('spec_helper')
+require_relative("../lib/todo")
+require("spec_helper")
 
-#TDD approach makes you write the test before you write the code. This can help you
-# create and write the methods before solving the problem
-describe "ToDoList" do 
+describe "Task" do 
+    
+    before :each do 
+        @task = Task.new("buy_milk")
+    end
 
-	before :each do 
-		todo_list = ToDoList.new
-	end
+    # describe "add" do 
+    #     it "should add a new task to todo array" do 
+    #         expect(@task.add(new_task)).to eq(array + new_task)
+    #     end
+    # end
+
+    describe "complete?" do
+        it "should determine if task is completed or not" do 
+            expect(@task.complete?).to eq(false)
+        end
+    end
+
+    describe "complete!" do 
+        it "should reverse status of task from false to true" do 
+            @task.complete!
+            expect(@task.complete?).to eq(true)
+        end
+    end
+
+    describe "make_incomplete!" do 
+        it "should reverse status of task from true to false" do
+            @task.make_incomplete! 
+            expect(@task.complete?).to eq(false)
+        end
+    end
+
+    describe "update_content!" do 
+        it "should update the task's text" do 
+            @task.update_content!("shopping")
+            expect(@task.content).to eq("shopping")
+        end
+    end
+
 end
-	describe "create_task" do 
-		it "It should create a task and add it to the array" do
-		expect(todo_list.create_task("Go Shopping")[0]).to eq("Go shopping") 
-	end
+
+describe "ToDoList" do
+
+    before :each do 
+        @task = Task.new("buy_milk")
+    end
+
+    describe "add_task" do 
+        it "should add new task to array of tasks" do 
+            @task.add_task
+            expect(@tasks.length).to eq(1)
+        end
+    end
+
 end
-  
