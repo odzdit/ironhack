05f1d1aa6dba0d772c48c9bab711eec47fef0efc
Dieter <odzdit@me.com>
2016-05-21 15:01:41 -0500

    iteration 3 todo list

--- week4/todo_list/lib/todo.rb
+++ week4/todo_list/lib/todo.rb
@@ -1,10 +1,12 @@
 class Task
     attr_reader :content, :id, :created_at
     @@current_id = 1
+    @@updated_at = nil
     def initialize(content)
         @content = content
         @id = @@current_id
         @created_at = Time.now
+        @updated_at = Time.now
         @@current_id += 1
     end
 end
