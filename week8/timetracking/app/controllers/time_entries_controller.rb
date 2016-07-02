09e064abe7ca926310dccf925ce2861323fc3a73
Dieter <odzdit@me.com>
2016-07-02 15:08:20 -0400

    we created the contact form and we also required the specific params to be included in the validation. The problem isthat we need to lock a project because otherwise a user can claim ownership of other projects

--- week8/timetracking/app/controllers/time_entries_controller.rb
+++ week8/timetracking/app/controllers/time_entries_controller.rb
@@ -8,4 +8,16 @@ class TimeEntriesController < ApplicationController
   	@project = Project.find(params[:project_id])
   	@time_entry = @project.time_entries.new
   end
+
+  def create
+	@project = Project.find(params[:project_id]) 
+	time_entry_params = params.require(:time_entry).permit(:hours, :minutes, :date)
+	@time_entry = @project.time_entries.new(time_entry_params)
+		if @time_entry.save
+		redirect_to project_time_entries_path(@project)
+	else
+		render "new"
+	end
+  end
+
 end
