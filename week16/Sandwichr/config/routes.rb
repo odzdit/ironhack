369948e6012abd4d3cc713cc4d88062b0d14df01
Dieter <odzdit@me.com>
2016-08-20 16:46:50 -0400

    added the users platorm to sandwich, learned about heroku

--- week16/Sandwichr/config/routes.rb
+++ week16/Sandwichr/config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
 
+  devise_for :users, path: '',
+  					 path_names: {sign_in: 'login', sign_up: 'signup'},
+  					 controllers: {sessions: 'devise/sessions'}
+
+  	root to: "sandwich_views#index"
+
+  get 'users/:id', to: 'users#show'
 resources :sandwiches, only: [:index, :show], controller: "sandwich_views"
 	scope "/api" do
 	  resources :ingredients
--- week16/Sandwichr/db/seeds.rb
+++ week16/Sandwichr/db/seeds.rb
@@ -5,7 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Ingredient.create!(name: "mustard", calories: 300 )
-Ingredient.create!(name: "tomato", calories: 15 )
-Ingredient.create!(name: "lettuce", calories: 20 )
-Ingredient.create!(name: "barbeque", calories: 200 )
+# Ingredient.create!(name: "mustard", calories: 300 )
+# Ingredient.create!(name: "tomato", calories: 15 )
+# Ingredient.create!(name: "lettuce", calories: 20 )
+# Ingredient.create!(name: "barbeque", calories: 200 )
+# Ingredient.create!(name: "bacon", calories: 3000 )
+# Ingredient.create!(name: "milanesa", calories: 700 )
+# Ingredient.create!(name: "arugula", calories: 10 )
+
+# Sandwich.create!(name: "JoshNats", bread_type: "lettuce bun" )
+# Sandwich.create!(name: "DA Joshting", bread_type: "potato bread" )
+# Sandwich.create!(name: "Joshandwich", bread_type: "" )
+# Sandwich.create!(name: "Joshub", bread_type: "bacon bread" )
+
+
+# clark, tony, rick, = User.create([
+# 	{name: 'dieter', email: "odzdit@me.com", password: "ironhack", password_confirmation: "ironhack"},
+# 	{name: 'granola', email: "granola@me.com", password: "ironhack", password_confirmation: "ironhack"},
+# 	{name: 'jorge', email: "jorge@me.com", password: "ironhack", password_confirmation: "ironhack"}
+# 		])	
